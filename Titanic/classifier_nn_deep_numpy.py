5cfbe4ab48ddb7893eff34e66ddafe42e9160fed
Juiwen Liu <Jui-wen.Liu@ou.edu>
2018-06-17 09:48:53 -0700

    Implement initialize_parameters()

---
+++ Titanic/classifier_nn_deep_numpy.py
@@ -0,0 +1,78 @@
+import copy
+from io import StringIO
+import matplotlib.pyplot as plt
+import numpy as np
+import re
+
+def main():
+    preProcessedData = preprocess_data()
+    cache = initialize_parameters(preProcessedData)
+    # cache = make_forward_propagation(cache)
+
+def preprocess_data():
+    with open('train.csv','r') as f:
+        rawRows = f.read()
+        rawRowsLastNameCommaStripped = re.sub(r'"(.*),(.*)"', r'\1 \2', rawRows) # drop comma between lastname and firstname
+
+    preProcessedData = np.genfromtxt(StringIO(rawRowsLastNameCommaStripped), delimiter= ',', dtype='|U64') # StringIO() is needed for a non-file-name object, see https://docs.scipy.org/doc/numpy-1.13.0/user/basics.io.genfromtxt.html Cast all cells to U64 string type to avoid structured ndarray (rank 1 array)
+    preProcessedData = preProcessedData[1:] # Drop header row
+    preProcessedData[:, 4] = (preProcessedData[:, 4] == 'male').astype(int) # In Sex column, male/female converted to 1/0, respectively
+    preProcessedData[:, 10] = (preProcessedData[:, 10] != '').astype(int) # In Cabin column, with/without data converted to 1/0, respectively
+    preProcessedData = preProcessedData[np.where(preProcessedData[:, 5] != '')] # Drop rows without data in Age column
+    preProcessedData = preProcessedData[:, [1, 2, 4, 5, 6, 7, 9, 10]] # Pick selected 8 columns for training (has to drop column 0 because huge numbers (for example, 891) will easily overflow the sigmoid evaluation)
+    preProcessedData = preProcessedData.astype(np.float) # Convert all cells to float
+    return preProcessedData
+
+def initialize_parameters(preProcessedData):
+    X = copy.deepcopy(preProcessedData[:, [1, 2, 3, 4, 5, 6, 7]]).T # Make deep copy to avoid corrupting raw data
+    Y = copy.deepcopy(preProcessedData[:, 0]).reshape(X.shape[1], -1).T # Make deep copy to avoid corrupting raw data. Reshape to address rank-1 array issue
+    m = X.shape[1] # Training Set count
+    N = [
+        X.shape[0], # Feature count
+        5, # Layer 1 neural unit count
+        4, # Layer 2 neural unit count
+        1  # Layer 3 neural unit (output) count
+    ]
+    L = len(N) # neural network layers count
+    W = [np.array([])] # empty array serves as dummy item
+    B = [np.array([])] # empty array serves as dummy item
+    A = [np.array([])] # empty array serves as dummy item
+    alpha = 0.001
+    epsilon = 0.000000001 # For divide-by-zero prevention
+
+    for i in range(1, L):
+        W.append(np.random.randn(N[i], N[i-1]))
+        B.append(np.zeros((N[i], 1)))
+        A.append(np.zeros((N[i], m)))
+
+    cache = {
+        'X': X,
+        'Y': Y,
+        'm': m,
+        'N': N,
+        'L': L,
+        'W': W,
+        'B': B,
+        'A': A,
+        'alpha': alpha,
+        'epsilon': epsilon
+    }
+
+    print('X: ' + str(X.shape))
+    print('Y: ' + str(Y.shape))
+    print('m: ' + str(m))
+    print('N: ' + str(N))
+    print('L: ' + str(L))
+
+    for i in range(L):
+        print('W' + str(i) + ': ' + str(W[i].shape))
+        print('B' + str(i) + ': ' + str(B[i].shape))
+        print('A' + str(i) + ': ' + str(A[i].shape))
+    
+    print('alpha: ' + str(alpha))
+    print('epsilon: ' + str(epsilon))
+    return cache
+
+
+
+main()
