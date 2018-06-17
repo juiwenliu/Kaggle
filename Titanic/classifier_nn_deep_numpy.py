21a65d34bcd4f5a66202bc84cf3adf2de7478fd6
Juiwen Liu <Jui-wen.Liu@ou.edu>
2018-06-17 14:44:44 -0700

    Fixed one-off issue on layer indexing before implementing make_backward_propagation()

--- Titanic/classifier_nn_deep_numpy.py
+++ Titanic/classifier_nn_deep_numpy.py
@@ -8,12 +8,10 @@ def main():
     preProcessedData = preprocess_data()
     cache = initialize_parameters(preProcessedData)
 
-    for i in range(100): # Training iterations
-        for j in range(1, cache['L']): # All layers iterations
-            make_forward_propagation(cache, j)
-
+    for i in range(cache['iterationsCount']): # Training iterations
+        make_forward_propagation(cache)
         compute_cost(cache)
-        print(str(i).rjust(20) + ": Cost = " + str(cache['cost'])) # Print progress with Cost
+        print(str(i).rjust(10) + ": Cost = " + str(cache['cost'])) # Print progress with Cost
 
 def preprocess_data():
     with open('train.csv','r') as f:
@@ -39,15 +37,16 @@ def initialize_parameters(preProcessedData):
         4, # Layer 2 neural unit count
         1  # Layer 3 neural unit (output) count
     ]
-    L = len(N) # neural network layers count
+    L = len(N) - 1 # neural network layers count. Minus one to exclude input layer
     W = [np.array([])] # empty array serves as dummy item
     B = [np.array([])] # empty array serves as dummy item
     A = [X] # X is regarded as A0
-    alpha = 0.001 # Learning rate
-    epsilon = 0.000000001 # For divide-by-zero prevention
+    alpha = np.power(10., -3) # Learning rate
+    epsilon = np.power(10., -10) # For divide-by-zero prevention and gradiant checking
+    iterationsCount = 10
 
-    for i in range(1, L):
-        W.append(np.random.randn(N[i], N[i-1]) * 0.01) # Randomly initializing neuros. Factor 0.01 is to ensure the starting parameter to be small to stay on linear zone (crucial for gradiant decent on sigmoid)
+    for i in range(1, L+1):
+        W.append(np.random.randn(N[i], N[i-1]) * np.power(10., -2)) # Randomly initializing neuros. Factor 0.01 is to ensure the starting parameter to be small to stay on linear zone (crucial for gradiant decent on sigmoid)
         B.append(np.zeros((N[i], 1)))
         A.append(np.zeros((N[i], m)))
 
@@ -61,7 +60,8 @@ def initialize_parameters(preProcessedData):
         'B': B,
         'A': A,
         'alpha': alpha,
-        'epsilon': epsilon
+        'epsilon': epsilon,
+        'iterationsCount': iterationsCount
     }
 
     print('X: ' + str(X.shape))
@@ -70,7 +70,7 @@ def initialize_parameters(preProcessedData):
     print('N: ' + str(N))
     print('L: ' + str(L))
 
-    for i in range(L):
+    for i in range(L+1):
         print('W' + str(i) + ': ' + str(W[i].shape))
         print('B' + str(i) + ': ' + str(B[i].shape))
         print('A' + str(i) + ': ' + str(A[i].shape))
@@ -79,29 +79,45 @@ def initialize_parameters(preProcessedData):
     print('epsilon: ' + str(epsilon))
     return cache
 
-def make_forward_propagation(cache, i):
+def make_forward_propagation(cache):
     W = cache['W']
     A = cache['A']
     B = cache['B']
-    Z = np.dot(W[i], A[i-1]) + B[i] # Z_l = W_l * A_l-1 + B_l
     L = cache['L']
 
-    if(i == (L - 1)):
-        A[i] = np.reciprocal(1 + np.exp(-Z)) # Apply sigmoid function 1 / (1 + e^-Z)
-    else:
+    for i in range(1, L): # All hidden layers (Layer 1 ~ L-1)
+        Z = np.dot(W[i], A[i-1]) + B[i] # Z_l = W_l * A_l-1 + B_l
         A[i] = np.maximum(0, Z) # Apply ReLU function max(0, Z)
 
+    # Output layer (Layer L)
+    Z = np.dot(W[L], A[L-1]) + B[L] # Z_L = W_L * A_L-1 + B_L
+    A[L] = np.reciprocal(1 + np.exp(-Z)) # Apply sigmoid function 1 / (1 + e^-Z)
     cache['A'] = A
 
-def compute_cost(cache): # Only compute top layer cost for now, since cost evaluation does not affect training, and cost evaluation slightly slow down training
+def compute_cost(cache): # Only compute top layer cost
     m = cache['m']
     A = cache['A']
     Y = cache['Y']
     L = cache['L']
     epsilon = cache['epsilon']
-    firstTerm  = np.dot(  Y, np.log(  A[L-1] + epsilon).T) # epsilon to avoid log(0) exception.
-    secondTerm = np.dot(1-Y, np.log(1-A[L-1] + epsilon).T) # epsilon to avoid log(0) exception
+    firstTerm  = np.dot(  Y, np.log(  A[L] + epsilon).T) # epsilon to avoid log(0) exception.
+    secondTerm = np.dot(1-Y, np.log(1-A[L] + epsilon).T) # epsilon to avoid log(0) exception
     cost = -(firstTerm + secondTerm) / m # per Cross Entropy Cost function. See https://en.wikipedia.org/wiki/Cross_entropy
     cache['cost'] = np.squeeze(cost) # np.squeeze to turn one cell array into scalar
+
+# def make_backward_propagation(cache, i):
+#     A = cache['A']
+#     w = cache['w']
+#     X = cache['X']
+#     Y = cache['Y']
+#     m = cache['m']
+
+#     # if(i == (L-1)):
+#     #     dZ = A - Y
+#     # else:
+
+
+#     cache['dw'] = np.dot(dZ, X.T) / m
+#     cache['db'] = np.sum(dZ, axis=1, keepdims=True) / m
 
 main()
