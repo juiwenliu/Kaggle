0b7a92c8c48c1b93085e9b607414afd76b87b44a
Juiwen Liu <Jui-wen.Liu@ou.edu>
2018-06-21 22:21:37 -0700

    Implemented logging and applied average age to all passengers without age data in order to apply learning on all 891 entries

--- Titanic/classifier_nn_deep_numpy.py
+++ Titanic/classifier_nn_deep_numpy.py
@@ -1,14 +1,15 @@
 import copy
+import datetime
 from io import StringIO
+import logging
 import matplotlib.pyplot as plt
 import numpy as np
 import re
+from scipy.special import expit
 
 def main():
     X, Y = preprocess_data()
-    cache = initialize_parameters(X, Y)
-    print('Iterations Cost') # Print progress with Cost
-    print('-----------------------------') # Print progress with Cost
+    cache, logger = initialize_parameters(X, Y)
 
     for i in range(cache['iterationsCount']): # Training iterations
         make_forward_propagation(cache)
@@ -17,13 +18,24 @@ def main():
         update_parameters(cache)
 
         if (np.remainder(i, 1000) == 0):
-            print(str(i).rjust(10) + ' ' + str(cache['cost'])) # Print progress with Cost
+            logger.info(str(i).rjust(10) + ' ' + str(cache['cost'])) # Log progress with Cost
+            print(str(i).rjust(10) + ' ' + str(cache['cost']))
+            cache['alpha'] *= cache['alphaDecay'] # Gradually ramp down learning rate when closing in to min
+
+            if (np.remainder(i, 100000) == 0):
+                W = cache['W']
+                B = cache['B']
+                L = cache['L']
+
+                for j in range(1, L+1):
+                    logger.info(W[j])
+                    logger.info(B[j])
 
     estimate_training_set_accuracy(cache)    
     
-    # W's aWnd B's are needed for prediction
-    print('W: ' + str(cache['B']))
-    print('B: ' + str(cache['B']))
+    # Final W's aWnd B's are needed for prediction
+    logger.info('W: ' + str(cache['W']))
+    logger.info('B: ' + str(cache['B']))
 
 def preprocess_data():
     with open('train.csv','r') as f:
@@ -32,12 +44,28 @@ def preprocess_data():
 
     preProcessedData = np.genfromtxt(StringIO(rawRowsLastNameCommaStripped), delimiter= ',', dtype='|U64') # StringIO() is needed for a non-file-name object, see https://docs.scipy.org/doc/numpy-1.13.0/user/basics.io.genfromtxt.html Cast all cells to U64 string type to avoid structured ndarray (rank 1 array)
     preProcessedData = preProcessedData[1:] # Drop header row
+
+    # Evaluate average age of known ages, and assign the same to those without known age
+    counter = 0
+    sum = 0
+
+    for i in range(preProcessedData.shape[0]):
+        if(preProcessedData[i, 5] != ''):
+            sum += preProcessedData[i, 5].astype(np.float64)
+            counter += 1
+
+    ageAverage = np.divide(sum, counter)
+
+    for i in range(preProcessedData.shape[0]):
+        preProcessedData[i, 5] = ageAverage if (preProcessedData[i, 5] == '') else preProcessedData[i, 5]
+
+    preProcessedData[:, 5] = np.divide(preProcessedData[:, 5].astype(np.float64), 10) # Age normalization to scale down age by one order of magnitude
+    preProcessedData[:, 9] = np.divide(preProcessedData[:, 9].astype(np.float64), 10) # Fare normalization to scale down age by one order of magnitude
     preProcessedData[:, 4] = (preProcessedData[:, 4] == 'male').astype(int) # In Sex column, male/female converted to 1/0, respectively
     preProcessedData[:, 10] = (preProcessedData[:, 10] != '').astype(int) # In Cabin column, with/without data converted to 1/0, respectively
-    preProcessedData = preProcessedData[np.where(preProcessedData[:, 5] != '')] # Drop rows without data in Age column
     preProcessedData = preProcessedData[:, [1, 2, 4, 5, 6, 7, 9, 10]] # Pick selected 8 columns for training (has to drop column 0 because huge numbers (for example, 891) will easily overflow the sigmoid evaluation)
-    preProcessedData = preProcessedData.astype(np.float) # Convert all cells to float
-    
+    preProcessedData = preProcessedData.astype(np.float64) # Convert all cells to float.
+
     X = copy.deepcopy(preProcessedData[:, [1, 2, 3, 4, 5, 6, 7]]).T # Make deep copy to avoid corrupting raw data
     Y = copy.deepcopy(preProcessedData[:, 0]).reshape(X.shape[1], -1).T # Make deep copy to avoid corrupting raw data. Reshape to address rank-1 array issue
     return X, Y
@@ -46,9 +74,10 @@ def initialize_parameters(X, Y):
     m = X.shape[1] # Training Set count
     N = [
         X.shape[0], # Feature count
-        32, # Layer 1 neural unit count
-        16, # Layer 2 neural unit count
-        8, # Layer 3 neural unit count
+        64, # Layer 1 neural unit count
+        64, # Layer 1 neural unit count
+        8, # Layer 1 neural unit count
+        8, # Layer 1 neural unit count
         1  # Layer 4 neural unit (output) count
     ]
     L = len(N) - 1 # neural network layers count. Minus one to exclude input layer
@@ -62,13 +91,15 @@ def initialize_parameters(X, Y):
     SdB = [np.array([])] # empty array serves as dummy item. SdB for ADAM optimization
     Z = [np.array([])] # empty array serves as dummy item
     A = [X] # X is regarded as A0
-    alpha = np.power(10., -4) # Learning rate
-    beta1 = 0.9 # Exponential decay hyperparameter for the first moment estimates used in ADAM optimization
-    beta2 = 0.999 # Exponential decay hyperparameter for the second moment estimates used in ADAM optimization
-    epsilon = np.power(10., -10) # For divide-by-zero prevention and ADAM optimization
-    iterationsCount = 10000
+    alpha = 3.446 * np.power(10., -4) # Learning rate
+    beta1 = 0.962 # Exponential alphaDecay hyperparameter for the first moment estimates used in ADAM optimization
+    beta2 = 0.99962 # Exponential alphaDecay hyperparameter for the second moment estimates used in ADAM optimization
+    alphaDecay = 0.995 # decay rate on Learning Rate 
+    epsilon = np.power(10., -7) # For divide-by-zero prevention and ADAM optimization
+    iterationsCount = 5000000
 
     for i in range(1, L+1):
+        np.random.seed(datetime.datetime.now().microsecond)
         W.append(np.random.randn(N[i], N[i-1]) * np.power(10., -2)) # Randomly initializing neuros. Factor 0.01 is to ensure the starting parameter to be small to stay on linear zone (crucial for gradiant decent on sigmoid)
         B.append(np.zeros((N[i], 1)))
         dW.append(np.zeros(W[i].shape))
@@ -99,33 +130,38 @@ def initialize_parameters(X, Y):
         'alpha': alpha,
         'beta1': beta1,
         'beta2': beta2,
+        'alphaDecay': alphaDecay,
         'epsilon': epsilon,
         'iterationsCount': iterationsCount
     }
 
-    print('X: ' + str(X.shape))
-    print('Y: ' + str(Y.shape))
-    print('m: ' + str(m))
-    print('N: ' + str(N))
-    print('L: ' + str(L))
+    logName = 'Logs/Log_' + str(datetime.datetime.now().strftime('%Y_%m%d_%H%M_%S%f')) + '.txt'
+    logger = logging.getLogger('logger')
+    logging.basicConfig(filename=logName, level=logging.INFO)
+    logger.info('X: ' + str(X.shape))
+    logger.info('Y: ' + str(Y.shape))
+    logger.info('m: ' + str(m))
+    logger.info('N: ' + str(N))
+    logger.info('L: ' + str(L))
 
     for i in range(L+1):
-        print('W' + str(i) + ': ' + str(W[i].shape))
-        print('B' + str(i) + ': ' + str(B[i].shape))
-        print('dW' + str(i) + ': ' + str(dW[i].shape))
-        print('dB' + str(i) + ': ' + str(dB[i].shape))
-        print('VdW' + str(i) + ': ' + str(VdW[i].shape))
-        print('VdB' + str(i) + ': ' + str(VdB[i].shape))
-        print('SdW' + str(i) + ': ' + str(SdW[i].shape))
-        print('SdB' + str(i) + ': ' + str(SdB[i].shape))
-        print('Z' + str(i) + ': ' + str(Z[i].shape))
-        print('A' + str(i) + ': ' + str(A[i].shape))
+        logger.info('W' + str(i) + ': ' + str(W[i].shape))
+        logger.info('B' + str(i) + ': ' + str(B[i].shape))
+        logger.info('dW' + str(i) + ': ' + str(dW[i].shape))
+        logger.info('dB' + str(i) + ': ' + str(dB[i].shape))
+        logger.info('VdW' + str(i) + ': ' + str(VdW[i].shape))
+        logger.info('VdB' + str(i) + ': ' + str(VdB[i].shape))
+        logger.info('SdW' + str(i) + ': ' + str(SdW[i].shape))
+        logger.info('SdB' + str(i) + ': ' + str(SdB[i].shape))
+        logger.info('Z' + str(i) + ': ' + str(Z[i].shape))
+        logger.info('A' + str(i) + ': ' + str(A[i].shape))
     
-    print('alpha: ' + str(alpha))
-    print('beta1: ' + str(beta1))
-    print('beta2: ' + str(beta2))
-    print('epsilon: ' + str(epsilon))
-    return cache
+    logger.info('alpha: ' + str(alpha))
+    logger.info('beta1: ' + str(beta1))
+    logger.info('beta2: ' + str(beta2))
+    logger.info('alphaDecay: ' + str(alphaDecay))
+    logger.info('epsilon: ' + str(epsilon))
+    return cache, logger
 
 # Implement forward propagation for the [LINEAR->RELU]*(L-1)->LINEAR->SIGMOID computation
 def make_forward_propagation(cache):
@@ -141,7 +177,7 @@ def make_forward_propagation(cache):
 
     # For output layer (Layer L)
     Z[L] = np.dot(W[L], A[L-1]) + B[L] # Z_L = W_L * A_L-1 + B_L
-    A[L] = np.reciprocal(1 + np.exp(-Z[L])) # Apply sigmoid function 1 / (1 + e^-Z)
+    A[L] = expit(Z[L]) # Apply sigmoid function 1 / (1 + e^-Z). Used to mitigate overflow of np.exp(). See http://arogozhnikov.github.io/2015/09/30/NumpyTipsAndTricks2.html
     cache['Z'] = Z
     cache['A'] = A
 
@@ -175,7 +211,7 @@ def make_backward_propagation(cache):
 	
     # Sigmoid backward-prop implementation. Only for output layer
     dAL = -np.divide(Y, A[L]+epsilon) + np.divide(1-Y, 1-A[L]+epsilon)
-    AL = np.reciprocal(1 + np.exp(-Z[L]))
+    AL = expit(Z[L]) # Apply sigmoid function 1 / (1 + e^-Z). Used to mitigate overflow of np.exp(). See http://arogozhnikov.github.io/2015/09/30/NumpyTipsAndTricks2.html
     dZ = dAL * AL * (1 - AL) # element-wise multiplication
     dW[L] = np.dot(dZ, A[L-1].T) / m
     dB[L] = np.sum(dZ, axis=1, keepdims=True) / m
@@ -216,6 +252,10 @@ def update_parameters(cache):
         W[i] -= alpha * VdW[i] / np.sqrt(SdW[i] + epsilon)
         B[i] -= alpha * VdB[i] / np.sqrt(SdB[i] + epsilon)
 
+    cache['VdW'] = VdW
+    cache['VdB'] = VdB
+    cache['SdW'] = SdW
+    cache['SdB'] = SdB
     cache['W'] = W
     cache['B'] = B
 
@@ -226,6 +266,6 @@ def estimate_training_set_accuracy(cache):
     Y = cache['Y']
     m = cache['m']
 
-    print('Rough accuracy on training set: ' + str(np.sum((np.abs(np.abs(A[L]) - Y) < 0.5).astype(int)) / float(m)))
+    logger.info('Rough accuracy on training set: ' + str(np.sum((np.abs(np.abs(A[L]) - Y) < 0.5).astype(int)) / float(m)))
 
 main()
